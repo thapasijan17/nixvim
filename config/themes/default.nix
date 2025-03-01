b05188510ef0fb49c20fa988aea31a00a408c447
thapasijan17 <thapasijan329@gmail.com>
2025-03-01 18:17:47 +0545

    just test

--- config/themes/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [
-    ./catppuccin.nix
-    # ./onedark.nix
-    # ./base16-radium.nix
-  ];
-  # config.extraConfigLua = ''vim.cmd("colorscheme catppuccin-mocha")'';
-}
--- config/themes/onedark.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}: {
-  colorschemes.onedark = {
-    enable = true;
-    # use the nvim variant instead of the vim variant
-    # The nvim variant is also treesitter enabled
-    package = pkgs.vimPlugins.onedark-nvim;
-  };
-}
