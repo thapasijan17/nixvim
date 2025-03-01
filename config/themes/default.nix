e6903adede29becdb784e930aad6df88c0e9f545
thapasijan17 <thapasijan329@gmail.com>
2025-03-01 18:39:35 +0545

    docs(nvim): add nix shell docs

--- config/themes/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [
-    ./catppuccin.nix
-    # ./onedark.nix
-    # ./base16-radium.nix # Only use this after commenting out the rest
-  ];
-  # config.extraConfigLua = ''vim.cmd("colorscheme catppuccin-mocha")'';
-}
